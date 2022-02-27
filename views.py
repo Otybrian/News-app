f49d2b2ee1d53369f3ade23a9a76968b849b0bf7
Otybrian <otieno.brian@student.moringaschool.com>
2022-02-28 02:08:27 +0300

    add readme

--- views.py
+++ views.py
@@ -20,15 +20,5 @@ def index():
 
     return render_template('index.html', title=title, header=header, top_news = top_news, our_sources=our_sources, news_source = news_source)
     
-# @app.route('/sources.html')
-# def sources():
-
-
-
-#     our_sources = 'News Sources'
-#     newsSource = get_news_sources('sources')
-#     print(newsSource)
-
-#     return  render_template('sources.html', our_sources=our_sources, newsSource = newsSource)
     
 
